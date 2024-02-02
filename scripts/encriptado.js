d5994470fe8827815d8ae55c3fa42d026dc5260b
Jason Morales <106495420+JasonMrR@users.noreply.github.com>
2024-02-02 14:08:16 -0600

    Se realizaron todas las funcionalidades requeridas

--- scripts/encriptado.js
+++ scripts/encriptado.js
@@ -1,7 +1,17 @@
-export const encriptaciones =  {
-  'a': '',
-  'e': '',
-  'i': '',
-  'o': '',
-  'u': '',
+const encriptado =  {
+  'a': 'ai',
+  'e': 'enter',
+  'i': 'imes',
+  'o': 'ober',
+  'u': 'ufat',
 }
+
+const desencriptado = {
+  'a': encriptado['a'].length -1,
+  'e': encriptado['e'].length -1,
+  'i': encriptado['i'].length -1,
+  'o': encriptado['o'].length -1,
+  'u': encriptado['u'].length -1,
+}
+
+export {encriptado, desencriptado};
